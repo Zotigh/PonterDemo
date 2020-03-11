8758e875aa83506599dd09190feabc3d865cd11a
Lance Zotigh <lancezotighlz@gmail.com>
2020-03-11 12:34:01 -0600

    Added the skeleton of the program

--- PonterDemo/PonterDemo.cpp
+++ PonterDemo/PonterDemo.cpp
@@ -2,10 +2,14 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-
+	double number1;
+	number1 = 6;
+	cout << "number1" << endl;
+	cout << "address of Number1" << &number1 << endl;
 	return 0;
 }
 
