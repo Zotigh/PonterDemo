cccfe5c694cb9c9c1b0fefcc18a741eda3cd5702
Lance Zotigh <lancezotighlz@gmail.com>
2020-03-11 12:40:18 -0600

    Back to skeleton

--- PonterDemo/PonterDemo.cpp
+++ PonterDemo/PonterDemo.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int main()
 {
-	double number1;
-	number1 = 6;
-	cout << "number1" << endl;
-	cout << "address of Number1: " << &number1 << endl;
-	
-	double* ptrNum;
-	ptrNum = &number1;
-	cout << "ptrNum: " << ptrNum << endl;
-	//cout << "ptrNum: " << &ptrNum << endl; This is the same as the top
-	cout << "ptrNum: " << *ptrNum << endl;
+	//double number1;
+	//number1 = 6;
+	//cout << "number1" << endl;
+	//cout << "address of Number1: " << &number1 << endl;
+	//
+	//double* ptrNum;
+	//ptrNum = &number1;
+	//cout << "ptrNum: " << ptrNum << endl;
+	////cout << "ptrNum: " << &ptrNum << endl; This is the same as the top
+	//cout << "ptrNum: " << *ptrNum << endl;
 
 	return 0;
 }
