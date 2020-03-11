842d819514793ad3015257716a3f2e6e724cf374
Lance Zotigh <lancezotighlz@gmail.com>
2020-03-11 12:39:22 -0600

    Added more logic

--- PonterDemo/PonterDemo.cpp
+++ PonterDemo/PonterDemo.cpp
@@ -9,11 +9,13 @@ int main()
 	double number1;
 	number1 = 6;
 	cout << "number1" << endl;
-	cout << "address of Number1" << &number1 << endl;
+	cout << "address of Number1: " << &number1 << endl;
 	
 	double* ptrNum;
 	ptrNum = &number1;
-	cout << "ptrNum" << ptrNum << endl;
+	cout << "ptrNum: " << ptrNum << endl;
+	//cout << "ptrNum: " << &ptrNum << endl; This is the same as the top
+	cout << "ptrNum: " << *ptrNum << endl;
 
 	return 0;
 }
